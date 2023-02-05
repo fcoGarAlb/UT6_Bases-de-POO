daf17905df16e0d4a046469aaa78aad1db4b6957
Francisco García Albaladejo <616193@alu.murciaeduca.es>
2023-02-05 23:50:40 +0100

    Agregar archivos de proyecto.

---
+++ Tarea_1_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra1, palabra2;
+
+            Console.Write("Dime una palabra: ");
+            palabra1 = Console.ReadLine();
+
+            Console.Write("Dime otra palabra: ");
+            palabra2 = Console.ReadLine();
+
+            if (palabra1 == palabra2)
+            {
+                Console.WriteLine("Las dos palabras son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Las dos palabras son distintas");
+            }
+        }
+    }
+}
