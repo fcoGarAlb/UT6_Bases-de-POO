44ad45ef2f436ea71b48b6ac32c7dd5e7e040100
Francisco García Albaladejo <616193@alu.murciaeduca.es>
2023-02-06 00:04:30 +0100

    Tarea_1_2 Hecha

---
+++ Tarea_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float G = 9.8f;
+            float tiempo;
+
+            Console.WriteLine("- Calculo de la velocidad en caida libre -");
+            Console.WriteLine("Velocidad = Gravedad (m/s^2)* tiempo (s)");
+            Console.Write("Dime el tiempo: ");
+            tiempo = float.Parse(Console.ReadLine());
+
+            if (tiempo <= 0)
+            {
+                Console.WriteLine("El tiempo de caida no puede ser cero, ni negativo");
+            }
+            else
+            {
+                Console.WriteLine("Velocidad = " + G + " * " + tiempo + " = " + (G * tiempo) + " m/s");
+            }
+        }
+    }
+}
