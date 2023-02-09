c2001c3ffefb07b66e9edb60327256ef1fd48d60
GARCIA ALBALADEJO, FRANCISCO <616193@alu365.murciaeduca.es>
2023-02-09 15:57:18 +0100

    Tarea 2_4 Hecha

---
+++ Tarea_2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numAleatorio = new Random();
+
+            Console.WriteLine("- Generador de números de la primitiva -\n");
+            for( int cont = 0; cont < 6; cont++)
+                Console.WriteLine( "  Número: " + numAleatorio.Next(1, 50));
+
+            Console.WriteLine("\nComplementario: " + numAleatorio.Next(1, 50));
+            Console.WriteLine("Reintegro: " + numAleatorio.Next(1, 10));
+        }
+    }
+}
