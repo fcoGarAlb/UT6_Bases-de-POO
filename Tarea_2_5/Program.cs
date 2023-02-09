ce1ae67eb6344b996ed3eb38552cc9bb6a9ba1c3
GARCIA ALBALADEJO, FRANCISCO <616193@alu365.murciaeduca.es>
2023-02-09 16:38:56 +0100

    Tarea 2_5 Hecha

---
+++ Tarea_2_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tarea_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime hoy = DateTime.Now;
+            DateTime fechaNacimiento;
+            int anyo, mes, dia;
+
+            Console.Write("Dime tu año de nacimiento: ");
+            anyo = int.Parse(Console.ReadLine());
+
+            Console.Write("Dime tu mes de nacimiento: ");
+            mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Dime tu día de nacimiento: ");
+            dia = int.Parse(Console.ReadLine());
+
+            fechaNacimiento = new DateTime(anyo, mes, dia);
+            TimeSpan diferencia = hoy.Subtract(fechaNacimiento);
+
+            Console.WriteLine($"\nTienes {Math.Truncate((diferencia.TotalDays / 365f))} años");
+
+
+        }
+    }
+}
