d2cb6cfd1b8bf1cb6d02f1f07e2beb6906877dde
GARCIA ALBALADEJO, FRANCISCO <616193@alu365.murciaeduca.es>
2023-02-16 17:15:51 +0100

    Por el ejercicio del calendario

---
+++ Tarea_3_6/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Solicita una fecha al usuario con día, mes y año y calcula el número de días que
+quedan para esa fecha. Si la fecha es anterior a hoy informale. Crea un método que
+dado un DateTime te devuelve el número de días que quedan para esa fecha, en
+caso de que sea una fecha pasada devuelve -1 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaUsuario;
+            int anyo, mes, dia, numDias;
+
+            Console.Write("Dime un año: ");
+            anyo = int.Parse(Console.ReadLine());
+
+            Console.Write("Dime un mes: ");
+            mes = int.Parse(Console.ReadLine());
+
+            Console.Write("Y un día: ");
+            dia = int.Parse(Console.ReadLine());
+
+            fechaUsuario = new DateTime(anyo, mes, dia);
+
+            switch (numDias = comparaFechas(fechaUsuario))
+            {
+                case -1: Console.WriteLine("La fecha es anterior a la actual"); break;
+                case  0: Console.WriteLine("Has introducido la fecha de hoy"); break;
+                default: Console.WriteLine($"Quedan {numDias} para esa fecha"); break;
+            }
+        }
+
+        static int comparaFechas(DateTime fechaUsuario)
+        {
+            int numDias = 0;
+            DateTime hoy = DateTime.Today;
+            TimeSpan diferencia = fechaUsuario.Subtract(hoy);
+            
+
+            numDias = (int) diferencia.Days;
+            
+            if (numDias < 0)     numDias = -1;
+
+            return numDias;
+        }
+    }
+}
---
+++ Tarea_3_7/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Crea una aplicación que muestre el calendario del mes actual. Utiliza un método
+para realizar la impresión por pantalla. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_7
+{
+    class Program
+    {
+        public enum MesesAnyo { Enero = 1, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Octubre, Noviembre, Diciembre };
+        static void Main(string[] args)
+        {
+            DateTime hoy = DateTime.Today;
+
+            ImprimeMesFecha(hoy);
+        }
+
+        static void ImprimeMesFecha(DateTime fecha)
+        {
+            int[,] mes = new int[5, 7];
+
+            int diaSemana = (int) fecha.DayOfWeek;
+            int numDiasMes = DateTime.DaysInMonth(fecha.Year, fecha.Month);
+            int cont = 1;
+
+            for (int s = 0; s < mes.GetLength(0) && cont <= numDiasMes; s++)
+            {
+                for (int d = 0; d < mes.GetLength(1) && cont <= numDiasMes; d++)
+                {
+                    mes[s, d] = cont++;
+                }
+            }
+
+            cont = 1;
+            Console.WriteLine((MesesAnyo) fecha.Month + " " + fecha.Year);
+            for (int s = 0; s < mes.GetLength(0) && cont <= numDiasMes; s++)
+            {
+                for (int d = 0; d < mes.GetLength(1) && cont <= numDiasMes; d++)
+                {
+                    Console.Write( mes[s, d] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
